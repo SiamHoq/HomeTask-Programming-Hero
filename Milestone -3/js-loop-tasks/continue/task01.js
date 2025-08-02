d616f2dc9b4105b399b7605b06097fb14eb627e6
Siam Haque <siamhoq@gmail.com>
2025-08-02 17:30:39 +0600

    continue updated

---
+++ Milestone -3/js-loop-tasks/continue/task01.js
@@ -0,0 +1,9 @@
+// Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+
+for(let n=1; n<=40; n++){
+    if(n%2===0){
+        continue;
+    }
+        console.log("numbers : ",n);
+
+}
